c2b6f203eb906153b958f60bbeafac5dd7f78075
michaelschulz <mschulz@creative-chaos.com>
2017-01-08 15:34:47 -0800

    Add files via upload

--- skccget.sh
+++ skccget.sh
@@ -7,6 +7,7 @@ if [ ! -d "$MEMBERDIR" ]; then
 
 fi
 
-wget http://www.skccgroup.com/search/skcclist.txt
+printf "SKCC\nStraight Key Century Club\n" > ~/.config/cqrlog/members/skcc.txt && curl http://www.skccgroup.com/search/skcclist.txt | sed 1d |sed s/\|/" "/g | sed s/call/""/ |sed s/skccnr/""/ |awk '{ print $2";"$1 }' |tee >> ~/.config/cqrlog/members/skcc.txt
 
-printf "SKCC\nStraight Key Century Club\n" > ~/.config/cqrlog/members/skcc.txt && cat skcclist.txt | sed 1d |sed s/\|/" "/g | sed s/call/""/ |sed s/skccnr/""/ |awk '{ print $2";"$1 }' |tee >> ~/.config/cqrlog/members/skcc.txt
+
+
